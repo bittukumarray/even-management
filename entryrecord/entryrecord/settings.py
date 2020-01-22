7d2f2f27313108a5da876fa470ba0310b7d63bce
bittukumarray <37700371+bittukumarray@users.noreply.github.com>
2020-01-22 21:14:00 +0530

    Update settings.py
    New change

--- entryrecord/entryrecord/settings.py
+++ entryrecord/entryrecord/settings.py
@@ -127,7 +127,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'rayb30909@gmail.com'
-EMAIL_HOST_PASSWORD = "1118209bittu"
+EMAIL_HOST_PASSWORD = "password"
 
 
 # Static files (CSS, JavaScript, Images)
