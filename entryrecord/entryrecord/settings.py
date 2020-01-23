ed42926f2305b5363597f96364d7bdee36653d77
bittukumarray <37700371+bittukumarray@users.noreply.github.com>
2020-01-23 10:12:20 +0530

    Update settings.py
    email and password updated.

--- entryrecord/entryrecord/settings.py
+++ entryrecord/entryrecord/settings.py
@@ -126,9 +126,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'rayb30909@gmail.com'
-EMAIL_HOST_PASSWORD = "password"
-
+EMAIL_HOST_USER = 'email'
+EMAIL_HOST_PASSWORD = 'password'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
